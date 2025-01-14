f303578420670a71dc733c94e60569bac332c471
Akash Patil <STSD21875AP@partner.bajajfinserv.in>
2025-01-14 12:10:42 +0530

    partytown enabled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Partytown } from '@builder.io/partytown';
 import './App.css';
 import Home from './component/Home';
 import Loans from './component/Loans';
@@ -48,6 +49,20 @@ function App() {
     //     </div>
     //   </header>
     // </div>
+    <div>
+    <Partytown forward={['dataLayer.push']} />
+    <script
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id=G-VF94F4403W' + i + dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','G-VF94F4403W');
+          `,
+        }}
+      ></script>
     <Router>
       <ButtonAppBar />
       <Routes>
@@ -56,6 +71,7 @@ function App() {
         <Route path="/aboutus" element={<Aboutus />} /> 
       </Routes>
     </Router>
+    </div>
   );
 }
 
