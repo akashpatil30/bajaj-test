448ae79e723e327cd94b69bc89ac87f93f23893c
Akash Patil <STSD21875AP@partner.bajajfinserv.in>
2025-03-03 15:44:10 +0530

    register s/w

---
+++ public/service-workder.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-undef
+importScripts('/~partytown/partytown-sw.js');
--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
 root.render(
   <React.StrictMode>
     <App />
